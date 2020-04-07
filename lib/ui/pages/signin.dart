d5eb4498f4da452a48263f4a962a997faffacf8d
Kanishka Bandara <kanishkaudaya99@gmail.com>
2020-04-07 20:39:58 +0530

    Add :: text field and logo.

--- lib/ui/pages/signin.dart
+++ lib/ui/pages/signin.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:swap_sell/ui/components/app_bar.dart';
 
 class Signin extends StatefulWidget {
   @override
@@ -10,11 +11,61 @@ class Signin extends StatefulWidget {
 class _SigninState extends State<Signin> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Signin'),
-          leading: IconButton(icon: Icon(Icons.arrow_back_ios),onPressed: ()=>Navigator.of(context).pop(),),
+    return Scaffold(
+      appBar:
+          ApplicationBar.createNormalAppBar(context, "Sign in", false, null),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 3,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(Icons.cake),
+                  Text("Swap&sell"),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 30,
+              ),
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  TextField(
+                    style: TextStyle(),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    style: TextStyle(),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  
+                  SizedBox(
+                    height: 20,
+                  ),
+                  RaisedButton(
+                    child: Text(
+                      "Sign in",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
