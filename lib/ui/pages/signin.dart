fb5351cb18e81fa83841e728ae60c00676ae112a
Kanishka Bandara <kanishkaudaya99@gmail.com>
2020-04-08 14:18:03 +0530

    Update :: change gap between signup button and lable

--- lib/ui/pages/signin.dart
+++ lib/ui/pages/signin.dart
@@ -19,8 +19,13 @@ class _SigninState extends State<Signin> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:
-          ApplicationBar.createNormalAppBar(context, "Sign in", false,false, null),
+      appBar: ApplicationBar.createNormalAppBar(
+        context,
+        "Sign in",
+        false,
+        false,
+        null,
+      ),
       body: GestureDetector(
         onTap: () => FocusScope.of(context).unfocus(),
         child: SingleChildScrollView(
@@ -28,7 +33,7 @@ class _SigninState extends State<Signin> {
             children: <Widget>[
               Container(
                 width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height / 4,
+                height: 140,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
@@ -144,11 +149,11 @@ class _SigninState extends State<Signin> {
                         ],
                       ),
                       SizedBox(
-                        height: 20,
+                        height: 30,
                       ),
                       Text("Do not have a profile?"),
                       SizedBox(
-                        height: 20,
+                        height: 10,
                       ),
                       FlatButton(
                         onPressed: () {},
