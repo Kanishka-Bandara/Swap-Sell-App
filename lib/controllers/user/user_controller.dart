24669956504f08b80316e6a93e35b21b779a0af2
Kanishka Bandara <kanishkaudaya99@gmail.com>
2020-04-13 23:45:52 +0530

    Add :: Create UserController Component -CRUD Operations.

--- lib/controllers/user/user_controller.dart
+++ lib/controllers/user/user_controller.dart
@@ -1,5 +1,21 @@
 import 'package:scoped_model/scoped_model.dart';
+import 'package:swap_sell/model/user/user.dart';
 
-class UserController extends Model{
-  
-}
+class UserController extends Model {
+  static UserController defaultUserController = UserController();
+
+  Future<User> createUser(User user) async {
+    //Save User on backend
+    return user;
+  }
+
+  Future<User> editUser(User user, int userID) async {
+    //Edit User on backend
+    return user;
+  }
+
+  Future<bool> deleteUser(int userID) async {
+    //Edit User on backend
+    return true;
+  }
+} 
