3d45bdf6c066db620923168207b33c98dd6df7e0
Kanishka Bandara <kanishkaudaya99@gmail.com>
2020-04-26 15:20:59 +0530

    Add :: Custom actions on the trailing.

--- lib/ui/components/app_bar.dart
+++ lib/ui/components/app_bar.dart
@@ -5,15 +5,12 @@ import 'package:swap_sell/controllers/plugins/g_cloud_vision_controller.dart';
 
 class ApplicationBar {
   static createHomeAppBar(BuildContext context) {
-    return AppBar(
-      title: Text('Swap&Sell'),
-      actions: <Widget>[
-        Container(
-          margin: EdgeInsets.only(right: 20),
-          child: AppIcons.createShoppingCart(context),
-        ),
-      ],
-      bottom: PreferredSize(
+    return createNormalAppBar(
+      context,
+      "Swap&Sell",
+      true,
+      false,
+      PreferredSize(
         preferredSize: Size(1000, 60),
         child: Container(
           margin: EdgeInsets.all(10),
@@ -77,142 +74,43 @@ class ApplicationBar {
           ),
         ),
       ),
+      null,
     );
   }
 
-  static createSearchAppBar(BuildContext context) {
-    return AppBar(
-      // automaticallyImplyLeading: false,
-      backgroundColor: Colors.white,
-      leading: IconButton(
-        icon: Icon(Icons.arrow_back),
-        onPressed: () {
-          Navigator.of(context).pop();
-        },
-      ),
-      actions: <Widget>[
-        TextField(),
-        IconButton(
-          icon: Icon(Icons.camera_alt),
-          color: Colors.grey,
-          onPressed: () {},
-        ),
-        IconButton(
-          icon: Icon(Icons.mic),
-          color: Colors.grey,
-          onPressed: () {},
-        )
-      ],
-    );
-  }
-
-//   static createSearchAppBar(BuildContext context) {
-//     return AppBar(
-//       automaticallyImplyLeading: false,
-//       bottom: PreferredSize(
-//         preferredSize: Size(1000, 20),
-//         child: Container(
-//           margin: EdgeInsets.all(10),
-//           height: 50,
-//           color: Colors.white,
-//           child: Row(
-//             // mainAxisAlignment: MainAxisAlignment.center,
-//             children: <Widget>[
-//               ButtonBar(
-//                 children: <Widget>[
-//                   IconButton(
-//                     icon: Icon(
-//                       Icons.arrow_back,
-//                       color: Colors.grey,
-//                     ),
-//                     onPressed: () {},
-//                   )
-//                 ],
-//               ),
-// // TextFormField(initialValue: "Search",),
-//               // GestureDetector(
-//               //   onTap: () {
-//               //     // Navigator.of(context).pushNamed(Routes.ROUTES_SEARCHPAGE);
-//               //   },
-//               //   child:
-//               //    Text(
-//               //     "Search for anything",
-//               //     style: TextStyle(fontSize: 20, color: Colors.grey),
-//               //   ),
-//               // ),
-//               Expanded(
-//                 child: Row(
-//                   mainAxisAlignment: MainAxisAlignment.end,
-//                   children: <Widget>[
-//                     IconButton(
-//                       icon: Icon(
-//                         Icons.mic,
-//                         color: Colors.grey,
-//                       ),
-//                       onPressed: () {},
-//                     ),
-//                     IconButton(
-//                       icon: Icon(
-//                         Icons.camera_alt,
-//                         color: Colors.grey,
-//                       ),
-//                       onPressed: () {},
-//                     ),
-//                   ],
-//                 ),
-//               ),
-//             ],
-//           ),
-//         ),
-//       ),
-//       // backgroundColor: Colors.white,
-//     );
-//   }
-
-  static createNormalAppBar(BuildContext context, String title,
-      bool showShoppingCart, bool showSearchIcon, Widget bottom) {
+  static createNormalAppBar(
+    BuildContext context,
+    String title,
+    bool showShoppingCart,
+    bool showSearchIcon,
+    Widget bottom, [
+    List<Widget> actions,
+  ]) {
     return AppBar(
       title: Text(title),
-      actions: <Widget>[
-        Container(
-          margin: EdgeInsets.only(right: 20),
-          child: Row(
-            children: <Widget>[
-              showSearchIcon
-                  ? IconButton(
-                      icon: Icon(Icons.search),
-                      onPressed: () {
-                        AppNavigator.navigateToSearchPage(context, "", false);
-                      })
-                  : Column(),
-              showShoppingCart
-                  ? AppIcons.createShoppingCart(context)
-                  : Column(),
-            ],
-          ),
-        )
-      ],
+      actions: actions == null
+          ? <Widget>[
+              Container(
+                margin: EdgeInsets.only(right: 20),
+                child: Row(
+                  children: <Widget>[
+                    showSearchIcon
+                        ? IconButton(
+                            icon: Icon(Icons.search),
+                            onPressed: () {
+                              AppNavigator.navigateToSearchPage(
+                                  context, "", false);
+                            })
+                        : Column(),
+                    showShoppingCart
+                        ? AppIcons.createShoppingCart(context)
+                        : Column(),
+                  ],
+                ),
+              )
+            ]
+          : actions,
       bottom: bottom,
-    );
-  }
-
-  static int _productPageBottomNavigationBarCurruntIndex;
-
-  static createProductPageBottomNavigationBar(BuildContext context) {
-    _productPageBottomNavigationBarCurruntIndex = 2;
-    return BottomNavigationBar(
-      currentIndex: _productPageBottomNavigationBarCurruntIndex,
-      items: [
-        BottomNavigationBarItem(icon: Icon(Icons.shop), title: Text("Shop")),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.add_shopping_cart), title: Text("Add")),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.check), title: Text("Buy Now")),
-      ],
-      onTap: (index) {
-        _productPageBottomNavigationBarCurruntIndex = index;
-        // setS
-      },
     );
   }
 }
